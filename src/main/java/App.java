d68fc814ec76e52994048495d53907b8d862932a
VitalyKamanin <vitalykamanin@gmail.com>
2022-09-14 14:20:10 +0300

    initial commit

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,13 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
+        HelloWorld hw1 =
                 (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+        HelloWorld hw2 =
+                (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(hw1 == hw2);
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+        System.out.println(cat1 == cat2);
     }
 }
